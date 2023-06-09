befcf17421d4b6bb26c7341f7a52af356b9cd842
SAURABH <saurabhdwivedi2310@gmail.com>
2023-06-09 22:19:17 +0530

    Category added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { PureComponent } from 'react'
 import NavBar from './Components/NavBar';
 import News from './Components/News';
+import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
 
 export default class App extends PureComponent {
   render() {
     return (
-      <div>
+      <div> 
+      <Router>
       <NavBar/>
-      <News pageSize={6} country= 'in' category='health'/>  
+      <Switch>
 
+          <Route exact path="/"><News  key="general" pageSize={6} country= 'in' category='general'/> </Route>
+          <Route exact path="/business"><News key="business"  pageSize={6} country= 'in' category='business'/> </Route>
+          <Route exact path="/entertainment"><News  key="entertainment" pageSize={6} country= 'in' category='entertainment'/> </Route>
+          <Route exact path="/health"><News  key="health" pageSize={6} country= 'in' category='health'/> </Route>
+          <Route exact path="/science"><News  key="science" pageSize={6} country= 'in' category='science'/> </Route>
+          <Route exact path="/sports"><News  key="sports" pageSize={6} country= 'in' category='sports'/> </Route>
+          <Route exact path="/technology"><News key="technology" pageSize={6} country= 'in' category='technology'/> </Route>
+
+      </Switch>
+      </Router>
       </div>
     )  
   }
 }
 
 //pageSize={5}   ye props hain hain jo hum kahi b use kre iski value yhi rhegi , news section me use kiya h
- 
+// working with     npm install react-router-dom@5.2.0
+// React dom version : 5.2.0
+
+// key="anything"   kuch bhi unique daldo else react har component ko ek jaisa smjh lega aur navigate krne k baad bhi reload ni hoga
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,5 +1,6 @@
-import PropTypes from 'prop-types'
 import React, { Component } from 'react'
+import {Link} from "react-router-dom";
+
 
 export class NavBar extends Component {
   static propTypes = {}
@@ -9,27 +10,25 @@ export class NavBar extends Component {
       <div>
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
     <div className="container-fluid">
-     <a className="navbar-brand" href="/">NewsMonkey</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+     <Link className="navbar-brand" to="/">NewsMonkey</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a> </li>
-
-        <li className="nav-item"> <a className="nav-link" href="/about">About</a>  </li>
-            <li className="nav-item"> <a className="nav-link" href="/about">Business</a> </li>
-            <li className="nav-item"> <a className="nav-link" href="/about">Entertainment</a>  </li>
-            <li className="nav-item"> <a className="nav-link" href="/about">General</a>  </li>
-            <li className="nav-item"> <a className="nav-link" href="/about">Health</a>  </li>
-            <li className="nav-item"> <a className="nav-link" href="/about">Science</a>  </li>
-            <li className="nav-item"> <a className="nav-link" href="/about">Sports</a>  </li>
-            <li className="nav-item"> <a className="nav-link" href="/about">Technology</a>  </li>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" to="/">Home</Link> </li>
+            <li className="nav-item"> <Link className="nav-link" to="/business">Business</Link> </li>
+            <li className="nav-item"> <Link className="nav-link" to="/entertainment">Entertainment</Link>  </li>
+            <li className="nav-item"> <Link className="nav-link" to="/general">General</Link>  </li>
+            <li className="nav-item"> <Link className="nav-link" to="/health">Health</Link>  </li>
+            <li className="nav-item"> <Link className="nav-link" to="/science">Science</Link>  </li>
+            <li className="nav-item"> <Link className="nav-link" to="/sports">Sports</Link>  </li>
+            <li className="nav-item"> <Link className="nav-link" to="/technology">Technology</Link>  </li>
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
       </form>
     </div>
   </div>
@@ -40,3 +39,9 @@ export class NavBar extends Component {
 }
 
 export default NavBar
+
+//here we will see adding react router for navigation in our page.
+
+// Use <Link , to=" ">  this prevent page loading
+// yha agr link ka error de toh import me daldena ,
+// import {Link} from "react-router-dom";
